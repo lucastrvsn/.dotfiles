61ab39b6dba0bfe62d5101ce048ee8a94a965e38
Lucas Trevisan <lucastrvsn@gmail.com>
2020-10-05 10:32:53 -0300

    post install

--- arch-install/post-install.sh
+++ arch-install/post-install.sh
@@ -13,18 +13,28 @@ echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo "cake" >> /etc/hostname
 echo "127.0.01 cake.localdomain  cake" >> /etc/hosts
 
+# root password
+echo "password for root:"
+passwd
+
+# create my user
+useradd -a -G wheel -s /usr/bin/zsh lucas
+sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+ALL\)/\1/' /etc/sudoers
+echo "password for lucas:"
+passwd lucas
+
 # configure home directory
 mkhomedir_helper lucas
 LC_ALL=C xdg-user-dirs-update --force
 
 # configure fonts
 mkdir -p /home/lucas/.config/fontconfig
-cp fontconfig.conf /home/lucas/.config/fontconfig/fonts.conf
+cp ./fontconfig.conf /home/lucas/.config/fontconfig/fonts.conf
 
 # install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --asroot
 rm -rf yay
 
 # install packages using yay
@@ -63,18 +73,9 @@ yay -S ${PACKAGES[@]}
 # initramfs
 mkinitcpio -P
 
-# root password
-passwd
-
 # grub install
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
 grub-mkconfig -o /boot/grub/grub.cfg
-
-# create my user
-useradd -m -G wheel,power,iput,storage,uucp,network -s /usr/bin/zsh lucas
-sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
-echo "password for lucas:"
-passwd lucas
 
 # setup services
 systemctl enable NetworkManager.service
